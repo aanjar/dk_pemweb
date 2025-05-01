f20c440279238f5340b24010bf08215a7184342d
farelfhr <farelfathir008@gmail.com>
2025-05-01 11:11:41 +0700

    Menambahkan file HTML, CSS, dan JS ke dalam resource dengan konten 2 page untuk beranda dan about us

---
+++ resources/js/scrollNavigation.js
@@ -0,0 +1,11 @@
+// Smooth scroll untuk semua anchor internal
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+  
