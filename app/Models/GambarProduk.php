14f9f902d695a5752a4ffee54267c7e6554eb09d
aanjar <aananjar956@gmail.com>
2025-05-01 23:16:36 +0700

    tambah model (Kategori, Produk, DetailProduk, GambarProduk)

---
+++ app/Models/GambarProduk.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GambarProduk extends Model
+{
+    use HasFactory;
+
+    protected $table = 'gambar_produk';
+
+    protected $fillable = ['id_produk', 'path_gambar'];
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class, 'id_produk', 'id');
+    }
+}
