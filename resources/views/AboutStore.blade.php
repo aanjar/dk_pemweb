5ac83ad22ab76f957ed7e93f28ef9f1675a82bb0
MReyAkbar <reyhanakbar90@gmail.com>
2025-05-15 12:58:16 +0700

    benerin typo

--- resources/views/AboutStore.blade.php
+++ resources/views/AboutStore.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/aboutStore.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/AboutStore.css') }}">
 </head>
 <body>
     <!-- Navigation -->
