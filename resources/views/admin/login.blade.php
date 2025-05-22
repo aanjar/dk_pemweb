315955c6b1311f8eacccb0d95ea450605d60d8cc
aanjar <aananjar956@gmail.com>
2025-05-22 15:01:49 +0700

    ubah shorcut icon

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin</title>
+    <link rel="shortcut icon" href="{{ asset('mainIMG/logoDK.png') }}" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -26,7 +27,7 @@
 <body>
     <div class="login-container">
         <h2 class="text-center">Login Admin</h2>
-        <h2 class="text-center mb-4">Dinoyo Camera</h2>
+        <h2 class="text-center mb-4">Dinoyo Kamera</h2>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf <div class="mb-3">
--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $produk->nama_produk }} - Detail Produk</title>
+    <title>{{ $produk->nama_produk }} - Detail Produk - Dinoyo Kamera</title>
+    <link rel="shortcut icon" href="{{ asset('mainIMG/logoDK.png') }}" type="image/png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
